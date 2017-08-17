c535c56e303d2d0aa252e9e3a8c363049f36db1f
aerodynamica <maximvlieghe10@hotmail.com>
2017-08-17 13:27:12 +0200

    Timer toegevoegd

--- QuizApp/QuizApp/EuhForm.vb
+++ QuizApp/QuizApp/EuhForm.vb
@@ -1,7 +1,11 @@
-﻿Public Class EuhForm
+﻿Imports System.Threading
+
+Public Class EuhForm
 
     Private CurrentPlayer As Player
     Private TimeLeft As Integer
+
+    Private CountdownThread As Thread
 
 
     Private Sub EuhForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -18,6 +22,21 @@
     End Sub
 
     Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        StartTimer()
+    End Sub
 
+    Private Sub StartTimer()
+        CountdownTimer.Start()
+    End Sub
+
+    Private Sub stopTimer()
+        CountdownTimer.Stop()
+    End Sub
+
+
+
+    Private Sub CountdownTimer_Tick(sender As Object, e As EventArgs) Handles CountdownTimer.Tick
+        TimeLeft -= 1
+        lblTimer.Text = TimeLeft.ToString
     End Sub
 End Class
